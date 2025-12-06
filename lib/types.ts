00f3f574deecfaf0b741a016a68e35cdb5e06fc6
trahwidhi <trahwidhi.oxtan17@gmail.com>
2025-12-06 13:45:02 +0800

    add COO VC

--- lib/types.ts
+++ lib/types.ts
@@ -91,7 +91,7 @@ export interface CreateDocumentResponse {
   signedW3CDocument: any;
 }
 
-export type DocumentId = 'ElectronicPromissoryNote' | 'WarehouseReceipt' | 'BillOfLading' | 'Invoice';
+export type DocumentId = 'ElectronicPromissoryNote' | 'WarehouseReceipt' | 'BillOfLading' | 'Invoice' | 'CertificateOfOrigin';
 
 export interface CreateDocumentResult {
   success: boolean;
@@ -422,12 +422,14 @@ export interface BillOfLading {
   collectCharges?: number;
 }
 
+/*
 export interface BillableItem {
   description?: string;
   quantity?: number;
   rate?: number;
   amount?: number;
 }
+*/
 
 export interface Invoice {
   invoiceId?: string;
@@ -467,4 +469,67 @@ export interface Invoice {
   tax?: number;
   taxTotal?: number;
   total?: number;
+}
+
+export interface CertificateOfOrigin {
+  cooId?: string;
+  issueDateTime?: string;
+  firstSignatoryAuthentication?: string;
+  signature?: string;
+  
+  // Supply Chain Consignment
+  supplyChainConsignment?: string;
+  supplyChainConsignmentId?: string;
+  supplyChainConsignmentInformation?: string;
+  
+  // Export Country
+  exportCountry?: string;
+  exportCountryCode?: string;
+  
+  // Exporter Information (flattened)
+  exporter?: string;
+  exporterId?: string;
+  exporterName?: string;
+  exporterLine1?: string;
+  exporterLine2?: string;
+  exporterCityName?: string;
+  exporterPostcode?: string;
+  exporterCountrySubDivisionName?: string;
+  exporterCountryCode?: string;
+  
+  // Import Country
+  importCountry?: string;
+  importCountryCode?: string;
+  
+  // Importer Information (flattened)
+  importer?: string;
+  importerId?: string;
+  importerName?: string;
+  importerLine1?: string;
+  importerLine2?: string;
+  importerCityName?: string;
+  importerPostcode?: string;
+  importerCountrySubDivisionName?: string;
+  importerCountryCode?: string;
+  
+  // Consignment Items
+  includedConsignmentItems?: string;
+  
+  // Loading Location
+  loadingBaseportLocation?: string;
+  loadingBaseportLocationId?: string;
+  loadingBaseportLocationName?: string;
+  
+  // Transport Movement
+  mainCarriageTransportMovement?: string;
+  mainCarriageTransportMovementId?: string;
+  mainCarriageTransportMovementInformation?: string;
+  usedTransportMeansName?: string;
+  usedTransportMeansId?: string;
+  departureDateTime?: string;
+  
+  // Unloading Location
+  unloadingBaseportLocation?: string;
+  unloadingBaseportLocationId?: string;
+  unloadingBaseportLocationName?: string;
 }
